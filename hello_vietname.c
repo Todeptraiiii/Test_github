864611a1e763757f377bc21adecb6a3a950004be
Todeptraii <daovanto@gmail.com>
2024-06-11 05:37:39 +0700

    revert to commit 106bced

--- hello_vietname.c
+++ hello_vietname.c
@@ -4,10 +4,13 @@ int main()
 {
     printf("Hello Vietname!");
     printf("Hello Vietname. Im single now!!");
+<<<<<<< HEAD
     printf("Hello NewBie");
 
     int a, b;
     a = b + 1;
     scanf("Nhap b : %d",&b);
     printf("Gia tri cua a la: %d \n",a);
+=======
+>>>>>>> parent of 106bced... Four commit on to branch
 }
