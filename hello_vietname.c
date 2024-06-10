0094b51b7e95ee144dcde7b8e9dd2611e889b42d
Todeptraii <daovanto@gmail.com>
2024-06-11 06:01:05 +0700

    Commit ngay 11-6

--- hello_vietname.c
+++ hello_vietname.c
@@ -4,5 +4,9 @@ int main()
 {
     printf("Hello Vietname!");
     printf("Hello Vietname. Im single now!!");
-    printf("Hello NewBie")
+    printf("Hello NewBie");
+
+    int a,b;
+    a = b + 1;
+    
 }
