885847bc85e8bf0b2c09e3887c77228edc0a1bbb
Todeptraii <daovanto@gmail.com>
2024-06-11 05:58:48 +0700

    Revert "Chinh sua 2 file tren nhanh togit add ."
    This reverts commit 1317221fbab9295d4dbc33327b6106d86b67bcc4.

--- hello_world.c
+++ hello_world.c
@@ -5,10 +5,4 @@ int main()
     printf("Hello World!");
     printf("Hello World. Im single now!!");
     printf("Hello to dep trai ");
-
-    int a = 1;
-    if (a == 1)
-    {
-        printf("A = 1");
-    }
 }
