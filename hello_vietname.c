304dac4df47d98fc40ab0ed88bcd66d9b4542051
Todeptraii <daovanto@gmail.com>
2024-06-11 05:43:34 +0700

    Revert lan 1

--- hello_vietname.c
+++ hello_vietname.c
@@ -5,12 +5,15 @@ int main()
     printf("Hello Vietname!");
     printf("Hello Vietname. Im single now!!");
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("Hello NewBie");
 
     int a, b;
     a = b + 1;
     scanf("Nhap b : %d",&b);
     printf("Gia tri cua a la: %d \n",a);
+=======
+>>>>>>> parent of 106bced... Four commit on to branch
 =======
 >>>>>>> parent of 106bced... Four commit on to branch
 }
