1317221fbab9295d4dbc33327b6106d86b67bcc4
Todeptraii <daovanto@gmail.com>
2024-06-11 05:13:34 +0700

    Chinh sua 2 file tren nhanh togit add .

--- hello_vietname.c
+++ hello_vietname.c
@@ -4,5 +4,10 @@ int main()
 {
     printf("Hello Vietname!");
     printf("Hello Vietname. Im single now!!");
-    printf("Hello NewBie")
+    printf("Hello NewBie");
+
+    int a, b;
+    a = b + 1;
+    scanf("Nhap b : %d",&b);
+    printf("Gia tri cua a la: %d \n",a);
 }
